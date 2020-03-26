a7d5a90b4f1068175fb6995cbddf9f7effb995a3
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2020-03-25 22:40:12 -0400

    First commit frontend

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Header({ title }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+import heroes from '../../assets/heroes.png';
+import logo from '../../assets/logo.svg';
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img src={logo} alt="Be the hero" />
+
+        <form>
+          <h1>Faça seu Login</h1>
+
+          <input placeholder="Sua ID" />
+          <button className="button" type="submit">
+            Entrar
+          </button>
+
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+
+      <img src={heroes} alt="Heroes" />
+    </div>
+  );
+}
